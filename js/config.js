dda003f0659ab84b93cf51fdccb9ccaae77156ee
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-01-08 13:09:03 +0100

    Changes to mirror OL version and OL-DEBUG version, added app.js for application logic.

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 
 
-scriptTag = '<script src="http://openlayers.org/en/v3.19.1/build/ol-debug.js" type="text/javascript"></script>';
+scriptTag = '<script src="http://openlayers.org/en/v4.6.4/build/ol-debug.js" type="text/javascript"></script>';
 document.write(scriptTag);
 
 scriptTag = '<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>';
