e1ed3e346b05f140a82ab3c0c03917542f5ef2b0
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-01-22 16:26:46 +0100

    Gestion du click sur les resultats de recherche

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ $(function(){
         source: new ol.source.Vector()
     });
     map.addLayer(resultat);
-    map.getView().setCenter(ol.proj.transform([6.15, 40], "EPSG:4326", "EPSG:3857"));
-    map.getView().setZoom(3);
+    map.getView().setCenter(ol.proj.transform([6.5, 46.5], "EPSG:4326", "EPSG:3857"));
+    map.getView().setZoom(4);
 
     $('#nomCabane').keyup(function (e) {
         if(e.keyCode == 13){
@@ -30,22 +30,15 @@ $(function(){
         }
     });
     $('.searchBtn').on('click',function(){
-        resultat.getSource().clear();
         mapboxClient.geocodeForward($('#nomCabane').val(),function(err,data,res){
             showResults(data);
-            var x = data.features[0].geometry.coordinates[0];
-            var y = data.features[0].geometry.coordinates[1];
+            var x = $('.results').find('.place').attr('data-x');
+            var y = $('.results').find('.place').attr('data-y');
             var center = [x,y];
-            var centerPSM = ol.proj.transform(center,"EPSG:4326", "EPSG:3857");
-            map.getView().setCenter(centerPSM);
-            map.getView().setZoom(12);
-            var feature = new ol.Feature({
-                geometry: new ol.geom.Point(centerPSM)
-            });
-            resultat.getSource().addFeature(feature);
+            showPlace(center);
             var cabanes = new ol.layer.Vector({
                 source: new ol.source.Vector({
-                    url:"http://pingouin.heig-vd.ch/~timothee.delapier/geoInf/getCabanes.php"+"?x="+centerPSM[0]+"&y="+centerPSM[1],
+                    url:"http://pingouin.heig-vd.ch/~timothee.delapier/geoInf/getCabanes.php"+"?x="+x+"&y="+y,
                     format: new ol.format.GeoJSON({
                         defaultDataProjection:"EPSG:3857"
                     })
@@ -76,12 +69,37 @@ $(function(){
 // TESTS
 function showResults(data){
   $('.results').empty();
-  $(data.features).each( function(index,element){
+  $(data.features).each( function(index,e){
     var result = RESULT_TEMPLATE.clone();
     result.removeClass('resTemplate');
-    result.find('a').html(element.place_name);
+    result.find('a').html(e.place_name);
+    x = e.geometry.coordinates[0];
+    y = e.geometry.coordinates[1];
+    center = convert3857([x,y]);
+    result.find('a').attr('data-x',center[0]);
+    result.find('a').attr('data-y',center[1]);
     result.appendTo('.results');
   })
 }
 
+function convert3857(center){
+  result = ol.proj.transform(center,"EPSG:4326", "EPSG:3857");
+  return result
+}
+
+function showPlace(center){
+  resultat.getSource().clear();
+  map.getView().setCenter(center);
+  map.getView().setZoom(12);
+  var feature = new ol.Feature({
+      geometry: new ol.geom.Point(center)
+  });
+  resultat.getSource().addFeature(feature);
+
+}
+$('.results').on('click','a',function(){
+  x = $(this).attr('data-x');
+  y = $(this).attr('data-y');
+  showPlace([x,y]);
+});
 });
