445971fcdbf9d6f7f5b05a6ac39492e9deb07081
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-01-22 15:35:55 +0100

    Affichage des resultats

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
  * Definition of global JS application parameters
  **************************************************/
 
@@ -7,6 +7,5 @@
 var scriptTag = '<script src="http://openlayers.org/en/v4.6.4/build/ol-debug.js" type="text/javascript"></script>';
 document.write(scriptTag);
 
-var scriptTag = '<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>';
+var scriptTag = '<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>';
  document.write(scriptTag);
-
